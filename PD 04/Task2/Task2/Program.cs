278025a06adecf6fea6611be8e17751d4acdc05f
Muhammad-Saad28 <2024CS216@student.uet.edu.pk>
2025-06-11 07:48:00 -0700

    final

---
+++ PD 04/Task2/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player Kangaroo = new Player("Kangaroo", 100, 40, 5);
+            Player Rabbit = new Player("Rabbit", 50, 20, 4);
+            Stats fireball = new Stats("Thuda", 23, 1.2F, 5, 15, "a magical attack");
+            Kangaroo.learnSkill(fireball);
+            Console.WriteLine(Kangaroo.attack(Rabbit));
+            Stats superBeam = new Stats("Mukka", 200, 50, 50, 75, "a cheap attack!!!");
+            Rabbit.learnSkill(superBeam);
+            Console.WriteLine(Rabbit.attack(Kangaroo));
+        }
+    }
+}
